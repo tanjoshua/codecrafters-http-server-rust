b80d67e8d2c716610b0ace4de7820aed9005ce67
josh <jtanjoshua@gmail.com>
2025-08-09 21:34:28 +0800

    respond with 404

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
-use std::io::Write;
-use std::net::TcpListener;
+use std::io::{BufRead, BufReader, Write};
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -9,14 +9,28 @@ fn main() {
 
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                println!("accepted new connection");
-                let resp = "HTTP/1.1 200 OK\r\n\r\n";
-                stream.write_all(resp.as_bytes()).unwrap();
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
             }
+        }
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    println!("accepted new connection");
+    let reader = BufReader::new(&stream);
+    let request = reader.lines().next().unwrap().unwrap();
+    match &request[..] {
+        "GET / HTTP/1.1" => {
+            let resp = "HTTP/1.1 200 OK\r\n\r\n";
+            stream.write_all(resp.as_bytes()).unwrap();
+        }
+        _ => {
+            let resp = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
+            stream.write_all(resp.as_bytes()).unwrap();
         }
     }
 }
